8988b180d831242a24320e679d8b1d728a2e8f1d
JackyLin2 <jacky.jia.lin@gmail.com>
2020-12-18 15:08:12 -0500

    about

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class About extends React.Component {
+
+
+    render() {
+       return (
+          <div className="about-container">
+              <div>
+                  <h1> About Me</h1>
+              </div>
+          </div>
+       );
+    }
+ }
+
+ export default About
