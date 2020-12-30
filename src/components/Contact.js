70c525e8f4d0c18f037ee09727af9e7316c0b5e6
JackyLin2 <jacky.jia.lin@gmail.com>
2020-12-29 22:49:29 -0500

    swal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
+import Swal from 'sweetalert2'
 
 
 class Contact extends React.Component {
+
+   state = {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+   }
+
+   handleSubmit = (e) => {
+      e.preventDefault()
+      emailjs.sendForm('service_xnro0bb', 'template_tstruct', e.target, 'user_nxAX5bsboFNtTwcx6luKA')
+      .then((event) => {
+         event.value = 'Send Email';
+         alert('Will Get Back To You ASAP!');
+      }, (error) => {
+          console.log(error.text);
+      });
+   }
+
 
     render() {
 
@@ -9,20 +30,20 @@ class Contact extends React.Component {
       return (
          <div id="Contact-container">
             <h3 className="Contact-Header"> Contact Me  </h3>
-            <div class="container">
-               <form action="/action_page.php">
-                  <label for="fname">First Name</label>
-                  <input type="text" id="fname" name="firstname" placeholder="Your name" />
+            <div className="container">
+               <form onSubmit={this.handleSubmit}>
+                  <label value="name">Name</label>
+                  <input type="text" name="name" placeholder="Your name" />
 
-                  <label for="lname">Last Name</label>
-                  <input type="text" id="lname" name="lastname" placeholder="Your last name" />
+                  <label value="email">E-Mail</label>
+                  <input type="e-mail" name="email" placeholder="Your E-Mail" />
 
 
-                  <label for="subject">Subject</label>
-                  <textarea id="subject" name="subject" placeholder="Your Message" style={{ height: 200 }} ></textarea>
+                  <label value="message">Message</label>
+                  <textarea name="message" placeholder="Your Message" style={{ height: "200px" }} ></textarea>
 
                   <input className="submit" type="submit" value="Submit" />
-               </form>
+               </form >
             </div>
          </div>
       );
