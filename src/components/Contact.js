15e26bd61491796fb62645bdf276832b8b875fd7
JackyLin2 <jacky.jia.lin@gmail.com>
2020-12-29 20:55:18 -0500

    contact me form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,12 +6,27 @@ class Contact extends React.Component {
     render() {
 
 
-       return (
-          <div id="Contact-container">
-                <h3 className="Contact-Header"> Contact Me  </h3>
-          </div>
-       );
-    }
- }
+      return (
+         <div id="Contact-container">
+            <h3 className="Contact-Header"> Contact Me  </h3>
+            <div class="container">
+               <form action="/action_page.php">
+                  <label for="fname">First Name</label>
+                  <input type="text" id="fname" name="firstname" placeholder="Your name" />
+
+                  <label for="lname">Last Name</label>
+                  <input type="text" id="lname" name="lastname" placeholder="Your last name" />
+
+
+                  <label for="subject">Subject</label>
+                  <textarea id="subject" name="subject" placeholder="Your Message" style={{ height: 200 }} ></textarea>
+
+                  <input className="submit" type="submit" value="Submit" />
+               </form>
+            </div>
+         </div>
+      );
+   }
+}
 
  export default Contact
